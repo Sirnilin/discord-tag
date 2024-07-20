02aac1668c7e7abdd9d1dd2b5c1b67246934a77c
Sirnilin <t89109753300@gmail.com>
2024-07-20 19:17:47 +0300

    скамлю бабушек

--- main.py
+++ main.py
@@ -5,35 +5,26 @@ import time
 import pyperclip
 from phrase import get_phrase
 
-enter_pressed = False
+
 
 def on_enter():
-    global enter_pressed
-    enter_pressed = True
+    text = get_phrase()
+    time.sleep(0.1)
+    pyperclip.copy(text)
+    
+    pyautogui.hotkey('shift', 'enter')
+    pyautogui.hotkey('shift', 'enter')
+    
+    pyautogui.hotkey('ctrl', 'v')
+    pyautogui.press('enter')
     
 def main():
-    global enter_pressed
-    
-    keyboard.add_hotkey('enter', on_enter, suppress=True)
-    
+
     while True:
         if not is_discord(get_active_window_title()): 
             continue
-        
+        keyboard.add_hotkey('enter', on_enter, suppress=True)
         keyboard.wait('enter')
-        
-        text = get_phrase()
-        time.sleep(0.1)
-        pyperclip.copy(text)
-        
-        pyautogui.hotkey('shift', 'enter')
-        pyautogui.hotkey('shift', 'enter')
-        
-        pyautogui.hotkey('ctrl', 'v')
-        
-        pyautogui.press('enter')
-
-        enter_pressed = False
 
 if __name__ == "__main__":
     main()
